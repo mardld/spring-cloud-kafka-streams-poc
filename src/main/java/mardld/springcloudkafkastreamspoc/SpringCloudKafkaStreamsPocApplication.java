4474b1f50f40996e514e716003d1422c9158e251
mardld <m.dailidonis@gmail.com>
2019-10-31 22:00:15 +0200

    add spring initializer project files

---
+++ src/main/java/mardld/springcloudkafkastreamspoc/SpringCloudKafkaStreamsPocApplication.java
@@ -0,0 +1,13 @@
+package mardld.springcloudkafkastreamspoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudKafkaStreamsPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudKafkaStreamsPocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mardld/springcloudkafkastreamspoc/SpringCloudKafkaStreamsPocApplicationTests.java
@@ -0,0 +1,13 @@
+package mardld.springcloudkafkastreamspoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudKafkaStreamsPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
